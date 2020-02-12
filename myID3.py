ee8da76c423cd3cfd57bee9ab5a19548b07c6551
imdar1 <13517002@std.stei.itb.ac.id>
2020-02-12 14:53:45 +0700

    Discrete ID3 completed

--- myID3.py
+++ myID3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import math
+from node import Node, Tree
 
 def entropy(data_target):
     # Create dictionary to store number of target in data
@@ -18,8 +19,11 @@ def entropy(data_target):
         return -9999
     else:
         result_entropy = 0
-        for target in target_dict:
-            result_entropy -= (target_dict[target]/n_total) * math.log(target_dict[target]/n_total) # Entropy formula
+        if len(target_dict) == 1:
+            return 0
+        else:
+            for target in target_dict:
+                result_entropy -= (target_dict[target]/n_total) * math.log(target_dict[target]/n_total) # Entropy formula
 
         return result_entropy
 
@@ -71,13 +75,36 @@ def info_gain(data_attr, data_target):
 
 def myID3(data, attributes, target_name):
     data_target = data[target_name]
+    
     # Data target contains one label
     if entropy(data_target) == 0:
-        print('DAUN ', data_target[0])
+        value_list = get_unique_data(data, target_name)
+        value_dict = dict()
+        for key, value in value_list.items():
+            value_dict[key] = len(value_list[key])
+
+        # Set current_node, info_gain, values
+        tree = Tree(Node(None,
+                         entropy(data_target), 
+                         value_dict, 
+                         result = data_target[0],
+                         is_leaf = True))
+        return tree
     
     # Nothing attribute shall be chosen
     if len(attributes) == 0:
-        print('DAUN ', most_label(data_target))
+        # Set current_node, info_gain, values
+        value_list = get_unique_data(data, target_name)
+        value_dict = dict()
+        for key, value in value_list.items():
+            value_dict[key] = len(value_list[key])
+
+        tree = Tree(Node(None,
+                         entropy(data_target),
+                         value_dict, 
+                         result = most_label(data_target),
+                         is_leaf = True))
+        return tree
     else:
         # Find best attribute to be node
         best_attr = ''
@@ -87,26 +114,35 @@ def myID3(data, attributes, target_name):
                 best_info_gain = info_gain(data[attr], data_target)
                 best_attr = attr
 
-        print('ATTR ', best_attr)
+        value_list = get_unique_data(data, target_name)
+        value_dict = dict()
+        for key, value in value_list.items():
+            value_dict[key] = len(value_list[key])
+
+        # Build decision tree recursively
+        dtree = Tree(Node(best_attr, best_info_gain, value_dict))
 
         # Delete usage attribute in attributes
         attributes.remove(best_attr)
         
         # Scan all posible value to be generated subtree
         list_attribute = get_unique_data(data, best_attr)
+        i = 0
         for attribute in list_attribute:
-            myID3(list_attribute[attribute], attributes, target_name)
+            data = pd.DataFrame(data=list_attribute[attribute]).reset_index(drop=True)
+            data.drop(best_attr, axis = 1, inplace=True)
+            dtree.add_child(myID3(data, attributes, target_name))
+            dtree.children[i].value.edge = attribute
+            i += 1
+        return dtree
+            
 
 
-
-# data = pd.read_csv("play_tennis.csv")
-# print('INFO GAIN OUTLOOK', info_gain(data['outlook'], data['play']))
-# print('INFO GAIN TEMP', info_gain(data['temp'], data['play']))
-# print('INFO GAIN HUMIDITY', info_gain(data['humidity'], data['play']))
-# print('INFO GAIN WIND', info_gain(data['wind'], data['play']))
-
-# myID3(data, ['outlook', 'temp', 'humidity', 'wind'], 'play')
-
-
-
-
+data = pd.read_csv("play_tennis.csv")
+print('INFO GAIN OUTLOOK', info_gain(data['outlook'], data['play']))
+print('INFO GAIN TEMP', info_gain(data['temp'], data['play']))
+print('INFO GAIN HUMIDITY', info_gain(data['humidity'], data['play']))
+print('INFO GAIN WIND', info_gain(data['wind'], data['play']))
+print(data)
+dtree = myID3(data, ['outlook', 'temp', 'humidity', 'wind'], 'play')
+dtree.print_tree()
--- node.py
+++ node.py
@@ -1,14 +1,48 @@
 # Node representation of decision tree
 
 class Node:
+    def __init__(self, current_node, info_gain, values, edge = None, result = None, is_leaf=False):
+        self.current_node = current_node
+        self.info_gain = info_gain
+        self.values = values
+        self.edge = edge
+        self.result = result
+        self.is_leaf = is_leaf
+    
+    def __str__(self):
+        node_str = ''
 
-    def __init__(self, value):
+        if self.edge is not None:
+            node_str += '['+ str(self.edge) +']'+'; '
+
+        if self.current_node is not None:
+            node_str += str(self.current_node)+'; ' 
         
+        if self.is_leaf:
+            node_str += 'ENTROPY: '
+        else:
+            node_str += 'INFO_GAIN: '
+        node_str += str(self.info_gain)
+
+        if self.result is not None:
+            node_str += '; RESULT: '+ str(self.result)
+        
+        node_str += '; VALUES: '+ str(self.values)
+
+        return node_str
+
+
+class Tree:
+
+    def __init__(self, value=None):
         self.value = value
         self.children = list()
 
     def add_child(self, node):
-        self.children.append(node)          
+        self.children.append(node)  
+
+    def set_node(self, node):
+        self.value = node
 
     def __text_builder(self, tree, depth):
         current_indent = ''
@@ -17,9 +51,9 @@ class Node:
         current_indent += '| '
 
         if not tree.children:
-            return current_indent+str(tree.value)+'\n'
+            return current_indent + str(tree.value) +'\n'
 
-        current_node = current_indent + str(tree.value)+'\n'
+        current_node = current_indent + str(tree.value) +'\n'
         for child in tree.children:
             current_node += str(self.__text_builder(child, depth+1))
         return current_node
