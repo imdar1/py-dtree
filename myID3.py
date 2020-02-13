dc1fac8c1378f0dada99865459f2fca7759926d0
Muhammad Fikri Hizbullah <13517104@std.stei.itb.ac.id>
2020-02-13 12:12:28 +0700

    make class MyID3

--- myID3.py
+++ myID3.py
@@ -3,111 +3,113 @@ import math
 from node import Node, Tree
 from calculate import Calculate
 
-def myID3(data, attributes, target_name, gain_ratio=False):
-    data_target = data[target_name]
-    
-    # Data target contains one label
-    entropy_data_target = Calculate.entropy(data_target)
-    if entropy_data_target == 0:
-        value_list = Calculate.get_unique_data(data, target_name)
-        value_dict = dict()
-        for key, value in value_list.items():
-            value_dict[key] = len(value_list[key])
+class MyID3():
+    def __init__(self, gain_ratio=False):
+        self.gain_ratio = gain_ratio
 
-        # Set current_node, info_gain, values
-        tree = Tree(Node(None,
-                         entropy_data_target, 
-                         value_dict, 
-                         result = data_target[0],
-                         is_leaf = True))
-        return tree
-    
-    # Nothing attribute shall be chosen
-    if len(attributes) == 0:
-        # Set current_node, info_gain, values
-        value_list = Calculate.get_unique_data(data, target_name)
-        value_dict = dict()
-        for key, value in value_list.items():
-            value_dict[key] = len(value_list[key])
 
-        tree = Tree(Node(None,
-                         entropy_data_target,
-                         value_dict, 
-                         result = Calculate.most_label(data_target),
-                         is_leaf = True))
-        return tree
-    else:
-        # Find best attribute to be node using either info gain or gain ratio
-        best_attr = ''
-        best_point = 0 # Could be Info gain or Gain ratio
-        for attr in attributes:
-            if gain_ratio:
-                point = Calculate.gain_ratio(data[attr], data_target)
-                if point > best_point:
-                    best_point = point
-                    best_attr = attr
-            else:
-                point = Calculate.info_gain(data[attr], data_target)
-                if point > best_point:
-                    best_point = point
-                    best_attr = attr
-
-        value_list = Calculate.get_unique_data(data, target_name)
-        value_dict = dict()
-        for key, value in value_list.items():
-            value_dict[key] = len(value_list[key])
-
-        # Build decision tree recursively
-        dtree = Tree(Node(best_attr, best_point, value_dict))
-
-        # Delete usage attribute in attributes
-        attributes.remove(best_attr)
+    def fit(self, data, attributes, target_name):
+        data_target = data[target_name]
         
-        # Scan all posible value to be generated subtree
-        list_attribute = Calculate.get_unique_data(data, best_attr)
-        i = 0
-        for attribute in list_attribute:
-            data = pd.DataFrame(data=list_attribute[attribute]).reset_index(drop=True)
-            data.drop(best_attr, axis = 1, inplace=True)
-            dtree.add_child(myID3(data, attributes, target_name, gain_ratio))
-            dtree.children[i].value.edge = attribute
-            i += 1
-        return dtree
+        # Data target contains one label
+        entropy_data_target = Calculate.entropy(data_target)
+        if entropy_data_target == 0:
+            value_list = Calculate.get_unique_data(data, target_name)
+            value_dict = dict()
+            for key, value in value_list.items():
+                value_dict[key] = len(value_list[key])
 
-def get_result(dtree, test):
-    if dtree.value.result is not None:
-        return dtree.value.result
-    
-    curr_attr = dtree.value.current_node
-    curr_edge = test[curr_attr].iloc[0]
-    
-    # Find index of the edge in the tree children
-    i = 0;
-    for child in dtree.children:
-        if curr_edge == child.value.edge:
-            break
+            # Set current_node, info_gain, values
+            tree = Tree(Node(None,
+                            entropy_data_target, 
+                            value_dict, 
+                            result = data_target[0],
+                            is_leaf = True))
+            return tree
+        
+        # Nothing attribute shall be chosen
+        if len(attributes) == 0:
+            # Set current_node, info_gain, values
+            value_list = Calculate.get_unique_data(data, target_name)
+            value_dict = dict()
+            for key, value in value_list.items():
+                value_dict[key] = len(value_list[key])
+
+            tree = Tree(Node(None,
+                            entropy_data_target,
+                            value_dict, 
+                            result = Calculate.most_label(data_target),
+                            is_leaf = True))
+            return tree
         else:
-            i += 1
+            # Find best attribute to be node using either info gain or gain ratio
+            best_attr = ''
+            best_point = 0 # Could be Info gain or Gain ratio
+            for attr in attributes:
+                if self.gain_ratio:
+                    point = Calculate.gain_ratio(data[attr], data_target)
+                    if point > best_point:
+                        best_point = point
+                        best_attr = attr
+                else:
+                    point = Calculate.info_gain(data[attr], data_target)
+                    if point > best_point:
+                        best_point = point
+                        best_attr = attr
 
-    return get_result(dtree.children[i], test)
+            value_list = Calculate.get_unique_data(data, target_name)
+            value_dict = dict()
+            for key, value in value_list.items():
+                value_dict[key] = len(value_list[key])
+
+            # Build decision tree recursively
+            dtree = Tree(Node(best_attr, best_point, value_dict))
+
+            # Delete usage attribute in attributes
+            attributes.remove(best_attr)
             
-def predict(dtree, data_test):
-    predicted_result = list()
- 
-    # Traverse through each row
-    for i in range(len(data_test)):
-        predicted_result.append(get_result(dtree, data_test.iloc[[i]]))
+            # Scan all posible value to be generated subtree
+            list_attribute = Calculate.get_unique_data(data, best_attr)
+            i = 0
+            for attribute in list_attribute:
+                data = pd.DataFrame(data=list_attribute[attribute]).reset_index(drop=True)
+                data.drop(best_attr, axis = 1, inplace=True)
+                dtree.add_child(self.fit(data, attributes, target_name))
+                dtree.children[i].value.edge = attribute
+                i += 1
+            return dtree
+
+    def _get_result(self, dtree, test):
+        if dtree.value.result is not None:
+            return dtree.value.result
+        
+        curr_attr = dtree.value.current_node
+        curr_edge = test[curr_attr].iloc[0]
+        
+        # Find index of the edge in the tree children
+        i = 0;
+        for child in dtree.children:
+            if curr_edge == child.value.edge:
+                break
+            else:
+                i += 1
+
+        return self._get_result(dtree.children[i], test)
+            
+    def predict(self, dtree, data_test):
+        predicted_result = list()
     
-    return predicted_result
+        # Traverse through each row
+        for i in range(len(data_test)):
+            predicted_result.append(self._get_result(dtree, data_test.iloc[[i]]))
+        
+        return predicted_result
         
 
 data = pd.read_csv("play_tennis.csv")
-# print('INFO GAIN OUTLOOK', Calculate.info_gain(data['outlook'], data['play']))
-# print('INFO GAIN TEMP', Calculate.info_gain(data['temp'], data['play']))
-# print('INFO GAIN HUMIDITY', Calculate.info_gain(data['humidity'], data['play']))
-# print('INFO GAIN WIND', Calculate.info_gain(data['wind'], data['play']))
-dtree = myID3(data, ['outlook', 'temp', 'humidity', 'wind'], 'play')
-dtree.print_tree()
+dTree = MyID3(gain_ratio=True);
+dtree_view = dTree.fit(data, ['outlook', 'temp', 'humidity', 'wind'], 'play')
+dtree_view.print_tree()
 
-hasil = predict(dtree, data[['outlook', 'temp', 'humidity', 'wind']])
+hasil = dTree.predict(dtree_view, data[['outlook', 'temp', 'humidity', 'wind']])
 print(hasil)
